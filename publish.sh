ab9ae70066e8f4e4220b03d56d41a0781cf0298f
Peter Hall <peterjoel@gmail.com>
2021-05-30 23:25:05 +0100

    fixed crate order in publish script

--- publish.sh
+++ publish.sh
@@ -4,8 +4,8 @@
 sleep 10s
 (cd macros; cargo publish) || exit $?
 sleep 10s
-(cd parse_pest; cargo publish) || exit $?
-sleep 10s
 (cd query; cargo publish) || exit $?
+sleep 10s
+(cd parse_pest; cargo publish) || exit $?
 sleep 10s
 cargo publish
