9f11724b36327ebcf165138b53b7ea53f38b75f8
Peter Hall <peterjoel@gmail.com>
2021-05-30 23:10:58 +0100

    v0.2.0

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash -e
+
+(cd core; cargo publish) || exit $?
+sleep 10s
+(cd macros; cargo publish) || exit $?
+sleep 10s
+(cd parse_pest; cargo publish) || exit $?
+sleep 10s
+(cd query; cargo publish) || exit $?
+sleep 10s
+cargo publish
