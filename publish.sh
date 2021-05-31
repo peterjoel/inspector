a727d3a320a17c48ba61acf6fbdee553ae859892
Peter Hall <peterjoel@gmail.com>
2021-05-31 15:22:38 +0100

    wait longer for crates.io to do its thing between publishes

--- publish.sh
+++ publish.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash -e
 
 (cd core; cargo publish) || exit $?
-sleep 10s
+sleep 25s
 (cd macros; cargo publish) || exit $?
-sleep 10s
+sleep 25s
 (cd query; cargo publish) || exit $?
-sleep 10s
+sleep 25s
 (cd parse_pest; cargo publish) || exit $?
-sleep 10s
+sleep 25s
 cargo publish
