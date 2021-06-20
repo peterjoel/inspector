1f9822b9921bb6d7e565df2aa11ac2deb883a8e4
Peter Hall <peterjoel@gmail.com>
2021-06-20 19:31:11 +0100

    tidied up the CLI

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,9 +1,8 @@
 mod console;
 use clouseau_core::Queryable;
-use clouseau_pest::parse_query;
+use clouseau_pest::{parse_query, Result};
 use clouseau_query::{Context, Query};
 use console::{Cli, CliResult};
-use itertools::Either;
 
 pub fn repl<'q, D: Queryable<'q>>(data: &'q D, ctx: &'q Context) {
     let mut cli = Cli::default();
@@ -12,14 +11,8 @@ pub fn repl<'q, D: Queryable<'q>>(data: &'q D, ctx: &'q Context) {
         state = match state {
             CliResult::Ready => cli.run(),
             CliResult::Quit => return,
-            CliResult::Done(query) => match parse_query(&query) {
-                Ok(query) => {
-                    // I have screwed up the lifetimes somewhere, but this is safe to do
-                    // Um.. the occasional segfaults say otherwise...
-                    let query: &'q Query = unsafe { &*(&query as *const _) };
-                    let results: Vec<_> = ctx.exec(&query, data as _).take(100).collect();
-                    cli.print_results(results)
-                }
+            CliResult::Done(query) => match run_query(data, ctx, &query) {
+                Ok(results) => cli.print_results(results),
                 Err(e) => cli.print_results(vec![format!("{}", e)]),
             },
             CliResult::Suggestions(query_part) => {
@@ -35,23 +28,22 @@ fn get_suggestions<'q, D: Queryable<'q>>(
     ctx: &'q Context,
     query: String,
 ) -> Vec<String> {
-    match query.chars().last() {
-        Some('/') => {
-            let query = if query.len() == 1 {
-                String::from(".keys()")
-            } else {
-                format!("{}.keys()", &query[..query.len() - 1])
-            };
-            run_query(data, ctx, &query).collect()
-        }
-        None => run_query(data, ctx, ".keys()")
-            .map(|k| format!("/{}", k))
-            .collect(),
-        Some('.') => ctx
-            .function_names()
+    if query.ends_with("./") {
+        run_query(data, ctx, &format!("{}.keys()", &query[..query.len() - 2]))
+            .ok()
+            .unwrap_or_else(Vec::new)
+    } else if query.ends_with('/') {
+        run_query(data, ctx, &format!("{}.keys()", &query[..query.len() - 1]))
+            .ok()
+            .unwrap_or_else(Vec::new)
+    } else if query.ends_with('.') {
+        ctx.function_names()
             .map(|name| format!("{}()", name))
-            .collect(),
-        _ => vec![],
+            .collect()
+    } else if query.is_empty() {
+        vec![String::from("/"), String::from("./"), String::from(".")]
+    } else {
+        Vec::new()
     }
 }
 
@@ -59,17 +51,18 @@ fn run_query<'q, D: Queryable<'q>>(
     data: &'q D,
     ctx: &'q Context,
     query: &str,
-) -> impl Iterator<Item = String> + 'q {
-    if let Ok(query) = parse_query(query) {
-        // I have screwed up the lifetimes somewhere, but this is safe to do
-        // Um.. the occasional segfaults say otherwise...
+) -> Result<Vec<String>> {
+    parse_query(query).map(|query| {
+        // I have probably screwed up the lifetimes somewhere for this to be necessary, but this is safe to
+        // do (provided that we don't do anything stupid, like return an iterator that references the query)
         let query: &'q Query = unsafe { &*(&query as *const _) };
         let mut results = ctx.exec(&query, data as _).peekable();
         if results.peek().is_some() {
-            return Either::Left(
-                std::iter::once(String::from("*")).chain(results.take(100).map(|d| d.to_string())),
-            );
+            std::iter::once(String::from("*"))
+                .chain(results.take(100).map(|d| d.to_string()))
+                .collect()
+        } else {
+            Vec::new()
         }
-    }
-    Either::Right(std::iter::empty())
+    })
 }
